58c7dc21496a92e4c9586970cc730a3545f3eeff
Isacc Barker <plutoisplanet4324@gmail.com>
2021-10-18 22:57:07 -0600

    fixed formatting to best of ability; can communicate with backend

---
+++ frontend/assets/js/index.js
@@ -0,0 +1,18 @@
+const backend_ip = "166.70.232.119:99"
+
+function check_textarea() {
+    let input = document.getElementById("post-inp").value;
+
+    document.getElementById("accuracy-rating").innerHTML = "...";
+
+    console.log("Querying server about:\n" + input);
+
+    // let response = fetch(backend_ip + "/query?q=" + encodeURI(input));
+    // lets fake it for now
+    let response = "{\"p\": 69}";
+
+    let json = JSON.parse(response);
+
+    document.getElementById("accuracy-rating").innerHTML = json['p'] + "%";
+}
+
