12bbc01e75351acda010f79e6a70de265d440fa2
Isacc Barker <plutoisplanet4324@gmail.com>
2021-10-19 02:46:02 -0600

    :(

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -1,4 +1,28 @@
-const backend_ip = "166.70.232.119:99"
+const backend_ip = "http://166.70.232.119:443"
+// const backend_ip = "https://ssl-proxy.my-addr.org/myaddrproxy.php/http/166.70.232.119:443" // Certbot didn't work, and I have limited time
+
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', "https://www.apple.com/", true);
+
+    xhr.withCredentials = true;
+    xhr.setRequestHeader("Access-Control-Allow-Credentials", "true");
+    xhr.setRequestHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    xhr.setRequestHeader('Access-Control-Allow-Methods', 'POST, GET, HEAD');
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+    xhr.responseType = 'text/plain';
+
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
+
 
 function check_textarea() {
     let input = document.getElementById("post-inp").value;
@@ -7,10 +31,37 @@ function check_textarea() {
 
     console.log("Querying server about:\n" + input);
 
-    let response = fetch(backend_ip + "/query?q=" + encodeURI(input));
+    let api_call = backend_ip + "/query?q=" + encodeURI(input);
+    const fetch_headers = new Headers({
+        'Origin': backend_ip,
+        'Access-Control-Allow-Origin': backend_ip,
+    });
 
-    let json = JSON.parse(response);
+    // getJSON(api_call, function(err, data) {
+    /* $.getJSON("https://www.apple.com/", function(data) {
+        console.log("Got:\n" + data);
 
-    document.getElementById("accuracy-rating").innerHTML = json['p'] + "%";
+        document.getElementById("accuracy-rating").innerHTML = data['p'] + "%";
+    }); */
+
+    $.ajax({
+    url: api_call,
+    type: 'GET',
+    // This is the important part
+    xhrFields: {
+        withCredentials: true
+    },
+    // This is the important part
+    // data: data,
+    success: function (response) {
+        // handle the response
+        let data = JSON.parse(response);
+        console.log("Got:\n" + data);
+        document.getElementById("accuracy-rating").innterHTML = data['p'] + '%';
+    },
+    error: function (xhr, status) {
+        // handle errors
+    }
+    });
 }
 
